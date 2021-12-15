ce05a5ba78882cb2827365dac17dc419c72e3797
bsbodollo01 <bsbodollo01@gmail.com>
2021-12-15 15:13:06 +0800

    initcommit

---
+++ application/app/Http/Middleware/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+use Auth;
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $t = \Auth::user()->acc_type;
+        
+        if ($t == 2) {
+            // nothing
+        } else {
+            Auth::logout();
+            return redirect()->route('denied');
+        }
+
+        return $next($request);
+    }
+}
---
+++ application/app/Http/Middleware/CheckStatus.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use View;
+use App\Classes\table;
+
+class CheckStatus
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $s = \Auth::user()->status;
+        $r = \Auth::user()->role_id;
+
+        if ($s == null || $s == 0) 
+        {
+            \Auth::logout();
+            return redirect()->route('disabled');
+        } 
+        
+        if ($r == null || $r == 0) 
+        {
+            \Auth::logout();
+            return redirect()->route('notfound');
+        }
+        
+        return $next($request);
+    }
+}
